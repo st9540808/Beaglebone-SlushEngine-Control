3cbf76caf5d99a4f6f9ecddbae2be6fb9b72ae2f
st9540808 <st9540808@gmail.com>
2018-06-17 22:48:31 +0800

    add main

---
+++ robot.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "slushboard.h"
+#include "slushmotor.h"
+
+
+int main(int argc, char const *argv[])
+{
+    SlushBoard board;
+    SlushMotor motor(0);
+
+    return 0;
+}
