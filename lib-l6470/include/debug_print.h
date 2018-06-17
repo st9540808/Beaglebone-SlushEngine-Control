ffb4934aecd9d6e4a18246bef39cfbc2dc0435f4
st9540808 <st9540808@gmail.com>
2018-06-17 22:50:57 +0800

    add debug print header

---
+++ lib-l6470/include/debug_print.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+
+#ifndef FATAL
+ #define FATAL(str)                                             \
+     do {                                                       \
+         fprintf(stderr, "errno %d, file %s (%d)\n",            \
+                 errno, __FILE__, __LINE__);                    \
+         perror(str); exit(1);                                  \
+     } while (0)
+#endif
+
+#ifndef DEBUG_PRINT
+ #ifdef DEBUG
+  #define DEBUG_PRINT(fmt, args...) fprintf(stderr, \
+     "DEBUG: %s(): " fmt "\n" , __func__, ##args)
+ #else
+  #define DEBUG_PRINT(fmt, args...) do {} while (0)
+ #endif
+#endif
